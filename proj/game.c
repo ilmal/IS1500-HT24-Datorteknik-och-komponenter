7fa1da0dc79f03ec6cf2d7b618fc72422ad443c0
Nils Malmberg <nils@u1.se>
2024-11-27 17:07:39 +0100

    adding stuff

--- proj/game.c
+++ proj/game.c
@@ -1,34 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum TYPE
+// Type of "room" the player can be in
+enum TILETYPE
 {
     // in the rocket
-    STORAGE,
-    CHAMBERS,
-    COCKPIT,
-    CAFETERIA,
-    ENGINE_BAY,
-    LABORATORY,
-    AIRLOCK,
+    STORAGE,        // 0
+    CHAMBERS,       // 1
+    COCKPIT,        // 2
+    CAFETERIA,      // 3
+    ENGINE_BAY,     // 4
+    LABORATORY,     // 5
+    AIRLOCK,        // 6
 
     // on the planet
-    LANDING_SITE,
-    WASTELAND,
-    LOOSE_SOIL,
-    POND,
-    SHARP_ROCKS,
-    CAVE,
-    CRATER,
-    CANYON,
-    MOUNTAIN,
+    LANDING_SITE,   // 7
+    WASTELAND,      // 8
+    LOOSE_SOIL,     // 9
+    POND,           // 10
+    SHARP_ROCKS,    // 11
+    CAVE,           // 12
+    CRATER,         // 13
+    CANYON,         // 14
+    MOUNTAIN,       // 15
+};
+
+const char* TILETYPE_NAMES[] = {
+    "STORAGE",
+    "CHAMBERS",
+    "COCKPIT",
+    "CAFETERIA",
+    "ENGINE_BAY",
+    "LABORATORY",
+    "AIRLOCK",
+    "LANDING_SITE",
+    "WASTELAND",
+    "LOOSE_SOIL",
+    "POND",
+    "SHARP_ROCKS",
+    "CAVE",
+    "CRATER",
+    "CANYON",
+    "MOUNTAIN"
 };
 
 struct tile
 {
-    enum TYPE type;   
-    int interactions;
+    enum TILETYPE type;   
+    // interactions
+    int interactable;
+    char interaction_text;
+
+    // storage
+    enum COLLECTIBLETYPE storage[10];
+
+    // collectibles
     int collectibles;
+
+    // stat changes
+    int drop_oxygen;
+    int drop_food;
+    int drop_water;
 };
 
 struct player {
@@ -36,8 +68,30 @@ struct player {
     int positionY;
     int food;
     int oxygen;
-    int inventory[10];
+    int inventory[5];
     int water;
+};
+
+enum COLLECTIBLETYPE {
+    TARDIGRADES,        // found in pond
+    SEDIMENTARY_LAYERS, // found in crater
+    RSL_IMAGES,         // found in canyon
+    ALIEN_BONES,        // found in cave
+    OLD_ROVER_PARTS,    // found in wasteland (low chance)
+    ICE,                // found in 
+    FOOD,               // found in cafeteria
+    BOTTLE_OF_WATER,    // found in cafeteria    
+};
+
+const char* COLLECTIBLE_NAMES[] = {
+    "Tardigrades",
+    "Sedimentary Layers",
+    "RSL Images"
+    "Alien Bones",
+    "Old Rover Parts"
+    "Ice",
+    "Food",
+    "Bottle of Water"
 };
 
 struct player* create_player() {
@@ -53,11 +107,29 @@ struct player* create_player() {
     return new_player;
 }
 
+struct tile* storage(){
+    struct tile* storage_tile = malloc(sizeof(struct tile));
+    storage_tile->interactable = 1;
+    storage_tile->interaction_text = "You are in the storage room. You can store your science samples here.";
+    memset(storage_tile->storage, 0, sizeof(storage_tile->storage));
+
+    // default items in the storage:
+    storage_tile->storage[0] = FOOD;
+    storage_tile->storage[1] = BOTTLE_OF_WATER;
+
+    return storage_tile;
+}
+
+
 // Constructor function to create a new tile (ct = create_tile)
-struct tile* ct(enum TYPE type) {
-    struct tile* new_tile = malloc(sizeof(struct tile));
-    new_tile->type = type;
-    return new_tile;
+struct tile* ct(enum TILETYPE type) {
+    switch (type)
+    {
+    case 0:
+        return storage();
+    default:
+        break;
+    }
 }
 
 
@@ -71,11 +143,12 @@ int main(){
     struct player* player = create_player();
 
     printf("Welcome to the game!\n");
-    printf("You are stranded on an Mars and need to find a way to survive.\n");
+    printf("You are stranded on Mars and need to find a way to survive.\n");
 
     while (1) {
         printf("You are at position (%d, %d)\n", player->positionX, player->positionY);
-        printf("What would you like to do?\n");
+        printf("The tile is: %s\n", TILETYPE_NAMES[map[player->positionY][player->positionX]->type]);
+        printf("What would you like to do? (write a number)\n");
         printf("1. Move\n");
         printf("2. Interact\n");
         printf("3. Check inventory\n");
